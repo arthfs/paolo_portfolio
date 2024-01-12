2ffb35b63079c42a50916eccfc9ea320254afe69
arthfs <arthursaintlouis96@gmail.com>
2024-01-11 20:32:34 -0500

    updated dimensions.js

--- src/pages/components/dimensions.js
+++ src/pages/components/dimensions.js
@@ -1,2 +1,10 @@
  var width = 300 ,height = 300;
  export {width,height}
+ import React from 'react'
+ 
+ export default function Dimensions() {
+   return (
+     <div>dimensions</div>
+   )
+ }
+ 
