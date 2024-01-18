72cad472dab5198729000003125dacbe2910896b
arthfs <arthursaintlouis96@gmail.com>
2024-01-18 12:32:07 -0500

    added new pictures

--- src/pages/components/necklaces.js
+++ src/pages/components/necklaces.js
@@ -6,16 +6,13 @@ import {width,height} from "../components/dimensions"
 export default function Necklaces() {
   return (
     <div className={Styles.bracelets}>
-        <Image className = {Styles.card}  src='/necklaces/pic1.webp' height={height} width={width} alt='pic1'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic2.webp' height={height} width={width} alt='pic2'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic3.webp' height={height} width={width} alt='pic3'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic4.webp' height={height} width={width} alt='pic4'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic5.jpg' height={height} width={width} alt='pic5'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic6.webp' height={height} width={width} alt='pic6'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic7.jpg' height={height} width={width} alt='pic7'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic8.webp' height={height} width={width} alt='pic8'></Image>
-        <Image className = {Styles.card}  src='/necklaces/pic9.webp' height={height} width={width} alt='pic9'></Image>
-</div> 
+        <Image className = {Styles.card}  src='/necklaces/pic1.jpeg' height={height} width={width} alt='pic1'></Image>
+        <Image className = {Styles.card}  src='/necklaces/pic2.jpeg' height={height} width={width} alt='pic2'></Image>
+        <Image className = {Styles.card}  src='/necklaces/pic3.jpeg' height={height} width={width} alt='pic3'></Image>
+        <Image className = {Styles.card}  src='/necklaces/pic4.jpeg' height={height} width={width} alt='pic4'></Image>
+        <Image className = {Styles.card}  src='/necklaces/pic5.jpeg' height={height} width={width} alt='pic5'></Image>
+        <Image className = {Styles.card}  src='/necklaces/pic6.jpeg' height={height} width={width} alt='pic6'></Image>
+    </div> 
 
 )
 }
--- src/pages/components/waistchains.js
+++ src/pages/components/waistchains.js
@@ -6,16 +6,13 @@ import {width,height} from "../components/dimensions"
 export default function Waistchains() {
   return (
     <div className={Styles.bracelets}>
-        <Image className = {Styles.card}  src='/waistchains/pic1.webp' height={height} width={width} alt='pic1'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic2.jpg' height={height} width={width} alt='pic2'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic3.webp' height={height} width={width} alt='pic3'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic4.webp' height={height} width={width} alt='pic4'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic5.webp' height={height} width={width} alt='pic5'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic6.jpg' height={height} width={width} alt='pic6'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic7.jpg' height={height} width={width} alt='pic7'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic8.jpg' height={height} width={width} alt='pic8'></Image>
-        <Image className = {Styles.card}  src='/waistchains/pic9.jpg' height={height} width={width} alt='pic9'></Image>
-</div> 
+        <Image className = {Styles.card}  src='/waistchains/pic1.jpeg' height={height} width={width} alt='pic1'></Image>
+        <Image className = {Styles.card}  src='/waistchains/pic2.jpeg' height={height} width={width} alt='pic2'></Image>
+        <Image className = {Styles.card}  src='/waistchains/pic3.jpeg' height={height} width={width} alt='pic3'></Image>
+        <Image className = {Styles.card}  src='/waistchains/pic4.jpeg' height={height} width={width} alt='pic4'></Image>
+        <Image className = {Styles.card}  src='/waistchains/pic5.jpeg' height={height} width={width} alt='pic5'></Image>
+        <Image className = {Styles.card}  src='/waistchains/pic6.jpeg' height={height} width={width} alt='pic6'></Image>
+    </div> 
 
 )
 }
